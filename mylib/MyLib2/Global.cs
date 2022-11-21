b54981007656f2553841659d883cfd928ee33fa2
dmitriy2k20 <118421220+dmitriy2k20@users.noreply.github.com>
2022-11-21 15:12:19 +0900

    Add files via upload

---
+++ mylib/MyLib2/Global.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyLib2
+{
+    public class Global
+    {
+        public void SayHello(string name)
+        {
+            Console.WriteLine($"Привет, {name}");
+        }
+        public int Sum(int x)
+        {
+            return x + 5;
+        }
+    }
+}
---
+++ mylib/mylib/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MyLib2;
+
+namespace mylib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Global func = new Global();
+
+            func.SayHello("Дима");
+            Console.WriteLine($"Сумма равна: {func.Sum(10)}");
+        }
+    }
+}
